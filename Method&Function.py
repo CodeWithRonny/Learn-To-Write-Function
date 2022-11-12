893fd730b27332006d3a80a7c056f1b7bc117bec
Ronny Dsouza <113218220+CodeWithRonny@users.noreply.github.com>
2022-11-12 09:48:02 +0530

    Add files via upload

---
+++ Method&Function.py
@@ -0,0 +1,30 @@
+
+# def codeWitRonny(a):
+# def codeWitRonny(a, b, c):
+# 	# return a+1
+# 	# return a + " ji"
+# 	# return a + b + c
+# 	# pass
+# 	print(a + b + c)
+
+# print(codeWitRonny([1, 2, 3]))
+# print(codeWitRonny(5))
+# print(codeWitRonny("Hello"))
+
+# print(codeWitRonny(2, 3, 5))
+
+# obj = codeWitRonny(2, 3, 5)
+# print(obj)
+
+
+
+def codeWitRonny(a, b, c):
+	# return a+1
+	# return a + " ji"
+	# return a + b + c
+	# pass
+	print(a + b - c)
+
+codeWitRonny(1, 2, 8)
+
+
